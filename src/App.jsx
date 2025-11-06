3bc6bd04ce4e1abee8c7dabfb1b3d9207bb672f6
kenneth234567 <bmckenlly@gmail.com>
2025-11-07 00:22:04 +0800

    Clean up App.jsx by removing comments
    Removed comments and unnecessary whitespace.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-// ✅ Components
 import Navbar from "./components/Navbar.jsx";
-
-// ✅ Pages
 import LandingPage from "./pages/LandingPage.jsx";
 import CarListing from "./pages/CarListing.jsx";
 import OrderPage from "./pages/OrderPage.jsx"; // Make sure this file exists!
@@ -12,10 +8,7 @@ import OrderPage from "./pages/OrderPage.jsx"; // Make sure this file exists!
 export default function App() {
   return (
     <Router>
-      {/* ✅ Navbar visible on all pages */}
       <Navbar />
-
-      {/* ✅ Define routes */}
       <Routes>
         <Route path="/" element={<LandingPage />} />
         <Route path="/cars" element={<CarListing />} />
